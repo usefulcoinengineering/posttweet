22f76d8ed4fe3bc72802c07218d486a4aa716825
munair <munair@gmail.com>
2022-08-05 21:21:17 -0700

    build on twitter's sample code.

---
+++ examples/example-credentials.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#
+# library name: credentials.py
+# library author: munair simpson
+# library created: 20220805
+# library purpose: store creditials privately/securely
+
+# To deploy:
+#   1. Replace these variables with your Gemini API credentials.
+#   2. Move the file to the "libraries" folder.
+#   3. Change the file name to "credentials.py"
+
+twitterapikey: AhgzZmARnNWznSMTrJB75tqUx
+twitterapisecret: EVYIzir1nvQQsITMid9H0PTOzu9zvuaAvQhmNRAJR6FXUlWPIa
